348b69f0eab83dfe554391feb1cb4c807692d593
Черемнов Константин Дмитриевич <k.cheremnov@tinkoff.ru>
2025-01-12 21:24:20 +0300

    fixing config-server

--- lab3/backend/soa-ebay/build.gradle.kts
+++ lab3/backend/soa-ebay/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
